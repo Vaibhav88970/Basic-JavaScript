1c0288d40489ca93c52ed6288411580a87e51d76
VaibhavKondapuram <98823781+VaibhavKondapuram@users.noreply.github.com>
2022-12-17 20:13:49 +0530

    Add files via upload

---
+++ basics/this2.js
@@ -0,0 +1,8 @@
+name:"Tutorial",
+            video["JavaScript", "this", "keyword"],
+            info()
+                this.video.forEach(function(tag){
+                    console.log(tag)
+                }),this
+            
+        random.info()
